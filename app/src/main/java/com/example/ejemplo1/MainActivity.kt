f0229874cd76081725ff3d0c59aa5b798a12f2d2
Juan Vaquera <juan.tec@outlook.com>
2024-09-04 13:39:18 -0600

    Se realizo cambios a la practica 1, en el cual seguimos los pasos de la seccion Mi Primer App hasta el ultimo punto.

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.ejemplo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     GreetingText(
                         message = "Feliz Cumpleaños Tec Laguna!",
-                        from = "De: Vaquera",
+                        from = "De: J. Vaquera",
                         modifier = Modifier.padding(8.dp)
                     )
                 }
