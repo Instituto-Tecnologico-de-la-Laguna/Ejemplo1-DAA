3aaf572045ca5d5274cd9d1b6de104f6054e4d71
Miguel Garza <miguelgrza.12@gmail.com>
2024-09-04 13:57:50 -0600

    Added row widget that surrounds texts

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,16 +37,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message
-    )
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Row {
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            lineHeight = 116.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejemplo1Theme{
-        GreetingText(message = "Feliz Cumpleaños Miguel!")
+        GreetingText(message = "Happy Birthday Miguel!", from = "From Maiky")
     }
 }
