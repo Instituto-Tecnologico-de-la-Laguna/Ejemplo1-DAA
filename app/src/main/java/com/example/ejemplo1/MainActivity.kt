3bab6f989dcce70842c15f5807837d6ad4688951
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-09-04 18:37:37 -0600

    Instrucción 7

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -1,40 +1,51 @@
 package com.example.ejemplo1
 
 import android.os.Bundle
+import android.os.Message
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
     }
+
+    private fun Surface(modifier: Modifier, color: Any, function: () -> Unit) {
+
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp
     )
 }
 
@@ -42,6 +53,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Ejemplo1Theme {
-        Greeting("Android")
+        GreetingText(message = "Feliz cumpleaños!", from = "De Isaac ")
     }
 }
