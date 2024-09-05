ecac4454412de34776214cafc719b30487c2e2c5
Miguel Garza <miguelgrza.12@gmail.com>
2024-09-04 19:47:34 -0600

    Created Message on app

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,32 +23,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Ejemplo1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Miguel!", from = "From Maiky")
                 }
             }
         }
-    }
-
-    private fun Surface(modifier: Modifier, color: Any, function: () -> Unit) {
-
     }
 }
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Row {
+    Column(modifier = modifier) {
         Text(
             text = message,
-            fontSize = 30.sp,
+            fontSize = 100.sp,
             lineHeight = 116.sp,
         )
         Text(
             text = from,
-            fontSize = 30.sp
+            fontSize = 36.sp
         )
     }
 }
