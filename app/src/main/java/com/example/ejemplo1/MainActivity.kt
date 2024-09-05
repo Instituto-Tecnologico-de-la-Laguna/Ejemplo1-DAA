fe9cb03a0a40f485e3771e607008eec44a1a1db8
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-09-04 23:00:46 -0600

    Practica modificada completada

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.ejemplo1
 import android.os.Bundle
 import android.os.Message
 import android.view.Surface
+import androidx.compose.material3.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +16,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 class MainActivity : ComponentActivity() {
@@ -28,37 +33,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //GreetingText(message = "Feliz cumpleaños Tec Laguna.", from = "Isaac Burciaga" )
+                    GreetingText(message = "Feliz cumpleaños Tec Laguna", from = "De Isaac Burciaga")
                 }
             }
         }
     }
     //
-    private fun Surface(modifier: Modifier, color: Any, function: () -> Unit) {
-
-    }
+//    private fun Surface(modifier: Modifier, color: Any, function: () -> Unit) {
+//
+//    }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejemplo1Theme {
-        GreetingText(message = "Feliz cumpleaños Tec Laguna!", from = "De Isaac ")
+        GreetingText(message = "Feliz cumpleaños Tec Laguna", from = "De Isaac Burciaga")
     }
 }
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
             text = message,
-            fontSize = 104.sp,
+            fontSize = 100.sp,
             lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 26.sp,
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
         )
     }
 }
