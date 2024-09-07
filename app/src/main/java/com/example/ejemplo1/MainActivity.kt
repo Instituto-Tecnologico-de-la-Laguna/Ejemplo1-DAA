7749de27b337a6577abbc287af9db8b3d0c5505a
IZimbra1 <alquimiero@hotmail.com>
2024-09-06 21:01:14 -0600

    Se han cambiado las funciones de texto, y se ha agregado la funcion GreetingImage

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ejemplo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,42 +32,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Feliz Cumpleaños Luis!",
-                        from = "From Jose",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    GreetingImage()
+                    BirthdayText(message = "Feliz Cumpleaños Jimmy", from = "Travis Scott")
                 }
             }
         }
     }
-}
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 60.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 26.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
-        )
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingTextPreview() {
-    Ejemplo1Theme {
-        GreetingText(message = "Feliz Cumpleaños Luis!", from = "From Jose")
+
+
+
+    @Composable
+    fun BirthdayText(message: String, from: String, modifier: Modifier = Modifier) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(
+                text = message,
+                fontSize = 55.sp,
+                lineHeight = 65.sp,
+                textAlign = TextAlign.Center
+
+            )
+
+            Text(
+                text = from,
+                fontSize = 25.sp,
+                modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
+            )
+        }
+
+    }
+
+    @Composable
+    fun GreetingImage(){
+        val image = painterResource(id = R.drawable.abstraction_paint_colorful_overlay)
+        Image(painter = image, contentDescription = null)
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BirthCardPreview(){
+        Ejemplo1Theme {
+            GreetingImage()
+            BirthdayText(message = "Feliz cumpleaños, Jimmy!", from = "Travis Scott")
+        }
     }
 }
