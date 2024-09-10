109e8f2d01ab4e8ade14cc3ceb2b5307eae50b69
Alejandro <alejandromartinezescobedo@gmail.com>
2024-09-10 10:47:41 -0600

    Actualización de proyecto con imagen de fondo y mensaje con diferentes características.

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.ejemplo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,7 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,6 +33,7 @@ import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
                 // A surface container using the 'background' color from the theme
@@ -33,9 +41,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Feliz Cumpleaños Luis!",
-                        from = "From Jose",
+                    GreetingImage(
+                        message = "Feliz Cumpleaños ITL!",
+                        from = "From AletsMarts",
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -43,6 +51,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
@@ -51,32 +61,45 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     ) {
         Text(
             text = message,
-            fontSize = 60.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            fontSize = 80.sp,
+            color = Color.White,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Thin,
+            fontFamily = FontFamily.Cursive
         )
         Text(
             text = from,
             fontSize = 26.sp,
+            color = Color.White,
+            fontFamily = FontFamily.Cursive,
             modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
         )
     }
 }
+
 @Composable
-fun GreetingImage(message: String, from: String){
-    val imagen= painterResource(id = R.drawable.pats)
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val imagen = painterResource(id = R.drawable._7206242960894)
     Image(
         painter=imagen ,
-        contentDescription = null
-    )
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        alpha = 0.9F,
+
+        )
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingTextPreview() {
     Ejemplo1Theme {
-        GreetingImage(message = "Feliz Cumpleaños Luis!", from = "From Jose")
+        GreetingImage(message = "Feliz Cumpleaños ITL!", from = "From AletsMarts")
+        GreetingText(message = "Feliz Cumpleaños ITL!", from = "From AletsMarts", modifier = Modifier.padding(8.dp))
     }
 }
